d2881d8169df1c062511d1a7f5891f1cf9175584
Siddharth8905 <siddharth8905@example.com>
2026-06-03 13:42:45 +0530

    cors error fixed

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: ["http://localhost:5173", "https://fincollect-app.vercel.app"] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
